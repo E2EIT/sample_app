27d1712b17e5013d65bf6e7f81ab321ba8ca3fd5
RCP <rproctor@e2eit.com>
2013-06-12 10:27:05 -0600

    Working User Signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,11 +3,40 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+
+  	it { should have_selector('h1',    text: user.name) }
+  	it { should have_selector('title', text: user.name) }
+	end  # End of Profile Page Tests
 
   describe "signup page" do
     before { visit signup_path }
+    
+    let(:submit) { "Create my account" }
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end  # End of tests with Invalid Information
+    
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end  # End of tests with Valid Information
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-end
+    it { should have_selector('title', text: 'Sign up') }
+  end  # End of Signup Page Tests
+end  # End of User Pages Tests
